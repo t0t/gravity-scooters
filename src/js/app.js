369d5cf3743d8ea2fdaaa33cf5c034fa28759898
Sergio Forés Raga <sergiofores@gmail.com>
2017-05-29 13:51:27 +0200

    init

---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+$("document").ready(function() {
+	// @codekit-prepend "popup.js", "main-nav.js", "auto-slideshow.js", "sticky-effect.js", "scroll-to-top.js";
+	console.log('funciona jquery');
+});
+
+
+
+
+(function() {
+
+let x = 100;
+let stringg = `
+	test ${x}
+`;
+console.log(stringg);
+	AOS.init({
+		duration: 1400
+	});
+
+})();
